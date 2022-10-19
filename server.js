8aa4ee8751565491839bffb3436dbd3bb1d5b9c1
Ruxin Qu <rosheenqu@gmail.com>
2022-10-18 22:07:32 -0700

    add comments to server.js increase the height of note title

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const generateUniqueId = require('generate-unique-id');
 const errorHandler = require('errorhandler');
 
+// middleware for all paths
 app.use(errorHandler())
 app.use(bodyParser.json())
 app.use(morgan('dev'))
@@ -13,20 +14,25 @@ app.use(express.static('public'));
 
 const PORT = process.env.PORT || 4001;
 
+// Returns index.html file 
 app.get('/', (req, res) => {
     res.sendFile('public/index.html', { root: __dirname });
 })
 
+// Returns notes.html file 
 app.get('/notes', (req, res) => {
     res.sendFile('public/notes.html', { root: __dirname });
 })
 
+// Returns all the notes from db.json file 
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         res.status(200).send(data);
     })
 })
 
+// Handles posted data. Create a newNote variable to store the data from the request body, parse the json
+// data from db.json and push the newNote to the array. Then stringify the array and write to the db.json file
 app.post('/api/notes', (req, res) => {
     const newNote = {
         id: generateUniqueId({
@@ -49,6 +55,8 @@ app.post('/api/notes', (req, res) => {
     }
 })
 
+// The id is parsed in req.params object. Find the element in the array with the id same from the request, then
+// delete that element from the array and stringify the array.
 app.delete('/api/notes/:id', (req, res) => {
     const id = req.params.id;
     if (id) {
